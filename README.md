6540849fbd75c785cbf64221c3c5f3182da49829
Mushimire Hubert <nyiringabohubert@gmail.com>
2019-05-30 21:36:12 +0100

    additional meaning of files

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<!--Introductory documentation for the root app-->
 # Ng6Rpwebsite
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 //If you open the file, you will see that the code has reference to different libraries, which are imported. Angular-cli has used these default libraries for the import - angular/core, platform-browser. The names itself explain the usage of the libraries
+//Defines the root module, named AppModule, that tells Angular how to assemble the application. Initially declares only the AppComponent. As you add more components to the app, they must be declared here
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
