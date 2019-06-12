bcaedc4d9641c696edd54fc4d0f42aea46d21837
Richard Mr <richard@yegobox.com>
2019-06-12 04:04:30 +0200

    make some correction and beginning phase, make sure app runs as usual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng6-rpwebsite';
+  title = 'Hubert i man';
+  firstNameO = "Muragijimana";
 }
